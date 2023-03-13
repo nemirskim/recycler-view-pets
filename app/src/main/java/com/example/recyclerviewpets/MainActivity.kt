cdcf307ac5c892f723ca95ce1e6d8ccc6205fa23
kot lena <aternetas@gmail.com>
2023-03-13 18:55:19 +0300

    add listener

--- app/src/main/java/com/example/recyclerviewpets/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewpets/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewpets.databinding.ActivityMainBinding
+import com.example.recyclerviewpets.services.PetListener
 import com.example.recyclerviewpets.services.PetService
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: PetAdapter
     private val petService: PetService
         get() = (applicationContext as App).petService
+    private val petListener: PetListener = {
+        adapter.pets = it
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,12 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         binding.rV.layoutManager = layoutManager
         binding.rV.adapter = adapter
+
+        petService.addListener(petListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        petService.removeListener(petListener)
     }
 }
